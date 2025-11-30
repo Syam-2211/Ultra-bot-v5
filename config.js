f5b69f248e7de90bc597675b6ab55371ae572984
Syam-2211 <syamsona2009@gmail.com>
2025-11-30 13:55:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
         prefix: '.',
-            ownerNumber: '919876543210@s.whatsapp.net', // REPLACE WITH YOUR NUMBER
+            ownerNumber: '919947121619@s.whatsapp.net', // REPLACE WITH YOUR NUMBER
                 botName: 'ULTRA BOT v5',
                     sessionID: process.env.SESSION_ID || '', // Leave empty for now
                         // Create a free database at MongoDB.com and paste URL here
-                            mongoURL: 'mongodb+srv://YOUR_MONGO_URL_HERE', 
-                                workMode: 'public', // 'private' or 'public'
+                            mongoURL: 'mongodb+srv://mongodb+srv://syamefx123:<@syamser123>.csjxapo.mongodb.net/', 
+                                workMode: 'private', // 'private' or 'public'
                                     
                                         // Auto-Reply Ad Settings
-                                            adName: 'MY BUSINESS',
-                                                adSlogan: 'Best Coding & Editing Tools',
-                                                    adLink: 'https://instagram.com/your_page'
+                                            adName: '🤍⃞𝄟ꪶ𝐒͢ʏ᪳ᴀ͓ᴍ͎ ͢𝐒ᴇ͓ꪳʀ͎𖦻⃞🍓',
+                                                adSlogan: 'HABIBBI ONLYENTERTAINMENTS..!',
+                                                    adLink: 'https://instagram.com/_mr.fro_ud_'
                                                     };
                                                     
 }
