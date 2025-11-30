a0d8032e189c828bd44ebf60ddc367bf86670de1
Syam-2211 <syamsona2009@gmail.com>
2025-11-30 14:19:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
 module.exports = {
-        prefix: '.',
-            ownerNumber: '919947121619@s.whatsapp.net', // REPLACE WITH YOUR NUMBER
-                botName: 'ULTRA BOT v5',
-                    sessionID: process.env.SESSION_ID || '', // Leave empty for now
-                        // Create a free database at MongoDB.com and paste URL here
-                            mongoURL: 'mongodb+srv://mongodb+srv://syamefx123:<@syamser123>.csjxapo.mongodb.net/', 
-                                workMode: 'private', // 'private' or 'public'
-                                    
-                                        // Auto-Reply Ad Settings
-                                            adName: '🤍⃞𝄟ꪶ𝐒͢ʏ᪳ᴀ͓ᴍ͎ ͢𝐒ᴇ͓ꪳʀ͎𖦻⃞🍓',
-                                                adSlogan: 'HABIBBI ONLYENTERTAINMENTS..!',
-                                                    adLink: 'https://instagram.com/_mr.fro_ud_'
-                                                    };
-                                                    
+    prefix: ".",
+    ownerNumber: "919947121619@s.whatsapp.net", // Your Number
+    ownerName: "𝑆𝑌𝛥𝛭.3𝐹𝛸", // Your Name
+    botName: "🤍⃞𝄟ꪶ𝐒͢ʏ᪳ᴀ͓ᴍ͎ ͢𝐒ᴇ͓ꪳʀ͎𖦻⃞🍓", // Your Bot Name
+    sessionID: process.env.SESSION_ID || "",
+    // If you have a MongoDB URL, paste it below inside the quotes
+    mongoURL: "mongodb+srv://user:pass@cluster.mongodb.net", 
+    workMode: "public",
+    
+    // Auto-Reply Settings (Updated with your details)
+    adName: "🤍⃞𝄟ꪶ𝐒͢ʏ᪳ᴀ͓ᴍ͎ ͢𝐒ᴇ͓ꪳʀ͎𖦻⃞🍓",
+    adSlogan: "HABIBBI ONLY ENTERTAINMENTS..!",
+    adLink: "https://instagram.com/_mr.fro_ud_"
 }
